4b7f855dbff25d1b923ebb2f4f11890f3f6eaefb
ahmad-ahmad <aa0796780751@gmail.com>
2021-04-14 16:54:51 +0300

    add

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const pg = require('pg');
 // const client = new pg.Client(process.env.DATABASE_URL);
 // const DATABASE_URL = process.env.DATABASE_URL;
 const client = new pg.Client( { connectionString: process.env.DATABASE_URL,
-  // ssl:{rejectUnauthorized: false
-  // }
+  ssl:{rejectUnauthorized: false
+  }
 
 } );
 
